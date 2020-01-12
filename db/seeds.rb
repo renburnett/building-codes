ed11d425ff15052e07675bcd45c9e3666a287f6b
renburnett <renardburnett@gmail.com>
2020-01-11 19:10:47 -0800

    oops forgot to move class method back into code.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,5 @@ require 'csv'
 # clear db before re-seeding
 Code.destroy_all
 
-def import_csv(csv_text, floor)
-  csv = CSV.parse(csv_text, :headers => true)
-  csv.each_with_index do |row|
-    Code.create(date_string: row['Day'], code: row['Code'], floor: floor)
-  end
-end
-
-import_csv(File.read(Rails.root.join('first.csv')), 'first')
-import_csv(File.read(Rails.root.join('second.csv')), 'second')
+Code.import_csv(File.read(Rails.root.join('first.csv')), 'first')
+Code.import_csv(File.read(Rails.root.join('second.csv')), 'second')
